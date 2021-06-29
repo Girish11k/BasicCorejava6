72fb31103bbada8d9d27b5a4ec220e62b6f4d98e
Girish11k <68158466+Girish11k@users.noreply.github.com>
2021-06-29 09:58:09 +0530

    commit perfectnumber push

---
+++ src/Com/perfect_Number/Perfect_Number.java
@@ -0,0 +1,29 @@
+package Com.perfect_Number;
+import java.util.Scanner;
+    class perfect_number_or_not
+    {
+        public static void main(String[] args)
+        {
+                        //scanner class declaration
+            Scanner sc = new Scanner(System.in);
+                        //input from user
+            System.out.print("Enter a number : ");
+
+            int number = sc.nextInt();
+                        //declare a variable to store sum of factors
+            int sum = 0;
+
+            for(int i = 1 ; i < number ; i++)
+            {
+                if(number % i == 0)
+                    sum = sum + i;
+            }
+                        //comparing whether the sum is equal to the given number or not
+            if(sum == number)
+                System.out.println("Perfect Number");
+            else
+                System.out.println("Not an Perfect Number");
+
+
+        }
+    }
